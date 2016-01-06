a34be1a9bc3caf3a1507273c1264e35609c1a421
arshad feeroz <arshadfeeroz2012@gmail.com>
2016-01-06 11:16:48 +0530

    Created Detail2.view

---
+++ view/Detail2.view.js
@@ -0,0 +1,16 @@
+sap.ui.jsview("sap.ui.final.test.view.Detail2",{
+	
+	getControllerName: function(){
+		return "sap.ui.final.test.view.Detail2"
+	},
+	//Initially the default detail page to be loaded
+	createContent: function(oController){
+		return new sap.m.Page({
+			content: [		
+									
+			]
+		});
+	}
+	
+});
+
